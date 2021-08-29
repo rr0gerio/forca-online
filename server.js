e8f48b30beabb70b7336de0e79097bd22c9bcdb5
Rogério Santos <rogeriosoares01@hotmail.com>
2021-08-29 02:34:22 -0300

    Adição de comando/controle de palavras e jogadores

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const crypto = require('crypto');
+
 
 const app = express();
 const server = require('http').createServer(app);
@@ -12,20 +14,87 @@ app.set('view engine', 'html');
 
 
 app.use('/', (req,res) =>{
-    res.render('index.html')
+    res.sendFile('index.html')
 });
 
-function criarPartida(){
-    const partida = {
+function createGame(io){
+    const game = {
+        palavras: {},
         jogadores: {},
-        novoJogador,
-        removerJogador
+        placar: {},
+        palavraCorrente=[],
+
+        adicionaPalavra, //add na partida
+        registraPalavraInfo, // broadcast para players
+        sortearPalavra, //sorteia uma pal
+
+        adicionaLetra,//add na partida
+        registraLetra,//broadcast para players
+        validaLetra,
+
+        adicionaJogador,
+        removeJogador,
+        
+        iniciarRodada
     }
+
+    
+    function adicionaPalavra(jogadorId, palavra){
+        var id = crypto.randomBytes(5).toString('hex');
+        return game.palavras[id] = {
+            palavra: palavra,
+            owner: jogadorId
+        }
+    }
+
+    function sortearPalavra(){
+        var keys = Object.keys(game.palavras);
+        var palavraObj =  game.palavras[
+            keys[keys.length * Math.random << 0]
+        ]
+        game.palavraCorrente=palavraObj.palavra.split("");
+        return{
+            tamanho: palavraObj.palavra.length,    
+            owner: palavraObj.owner
+        } 
+    }
+
+    function registraPalavraInfo(){
+        io.broadcast.emit('nova-palavra',sortearPalavra)
+    }
+
+
+    function adicionaLetra(jogador,letra,timestamp){
+        
+    }
+
+    function registraLetra(){}
+    function validaLetra(){}
+
+    function adicionaJogador(jogadorId, nomeUsuario){
+        game.jogadores[jogadorId]={
+            nome: nomeUsuario,
+            pontuacao: 0
+        }
+        io.broadcast.emit('registra-novo-jogador',game.jogadores[jogadorId]);
+    }
+
+    function removeJogador(jogadorId){
+        io.broadcast.emit('remove-jogador',jogadorId);
+        delete game.jogadores[jogadorId]
+    }
+
+    function iniciarRodada(){}
+
+    return game;
 }
 
-io.on('connection', socket => {
+
+
+const game = createGame(io);
+io.on('connection', (socket) => {
     console.log(`${socket.id}`);
-    
+
     socket.on('novaLetra', novaLetra => {
         // validar letra com a palavra corrente
         // se a letra for valida
@@ -35,16 +104,14 @@ io.on('connection', socket => {
         console.log('nova letra');
     });
     
-    socket.on('novaPalavra', novaPalavra =>{
+    socket.on('novaPalavra', (novaPalavra) =>{
         // inserir nova palavra no array
-        console.log('nova palavra');
+        game.adicionaPalavra(socket.id, novaPalavra);
+        console.log(game.palavras);
     })
-    socket.on('novoJogador', nomeUsuario =>{
+    socket.on('novoJogador', (nomeUsuario) =>{
         //inserir novo jogador socket.id + username
-        jogador={
-            username: nomeUsuario,
-            id: socket.id
-        } 
+        game.adicionaJogador(socket.id, nomeUsuario);
         console.log(jogador);
     })
 });
